d07861289485440b18e1799ca9f62c21ef093112
Ubuntu <29393345+hivanco@users.noreply.github.com>
2020-03-23 14:35:34 -0400

    Server dockerfile removed sleep time, expose port. Server takes argument for port to listen on. Client tajes argument for port to contact proxy.

--- helloworld/helloworldclient/helloworldclient.go
+++ helloworld/helloworldclient/helloworldclient.go
@@ -4,18 +4,27 @@ import (
     "bufio"
     "fmt"
     "net/http"
+    "os"
 	"time"
 )
 
 func main() {
+    if len(os.Args) != 2 {
+        fmt.Println("Usage:", os.Args[0], "PORT")
+        os.Exit(1)
+    }
+
+    port := os.Args[1]
 	pollInterval := 2
 	timerCh := time.Tick(time.Duration(pollInterval) * time.Second)
 	// do every 2 seconds
 	for range timerCh {
 		// make request
-        resp, err := http.Get("http://127.0.0.1:4201/hello-world-server")
+        resp, err := http.Get("http://127.0.0.1:" + port + "/hello-world-server")
         if err != nil {
-            panic(err)
+            // panic(err)
+            fmt.Println(err.Error())
+            continue
         }
 
         fmt.Println("Response status:", resp.Status)
--- helloworld/helloworldserver/helloworldserver.go
+++ helloworld/helloworldserver/helloworldserver.go
@@ -4,13 +4,21 @@ import (
     "fmt"
     "log"
     "net/http"
+    "os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("Got request")
     fmt.Fprintf(w, "Hello, World\n")
 }
 
 func main() {
+    if len(os.Args) != 2 {
+        fmt.Println("Usage:", os.Args[0], "PORT")
+        os.Exit(1)
+    }
+
+    port := os.Args[1]
     http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    log.Fatal(http.ListenAndServe(":" + port, nil))
 }
