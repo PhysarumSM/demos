d8c994625426ff35e4fefe5b4b4e3b35c19302c8
Ubuntu <29393345+hivanco@users.noreply.github.com>
2020-04-29 17:47:18 -0400

    updated webpage

--- covid19-tracker/covid19-webserver/covid19-webserver.go
+++ covid19-tracker/covid19-webserver/covid19-webserver.go
@@ -32,14 +32,7 @@ const homepage string =
     <h2>Date</h2>
     <p>Leave blank to get latest date</p>
 
-    <label for="month">Month (1-12):</label><br>
-    <input type="text" id="month" name="month" value=""><br>
-
-    <label for="day">Day:</label><br>
-    <input type="text" id="day" name="day" value=""><br>
-
-    <label for="year">Year:</label><br>
-    <input type="text" id="year" name="year" value=""><br>
+    <input type="date" id="date" name="date" min="2020-01-22"><br>
     <br>
 
     <input type="submit" value="Submit">
@@ -60,9 +53,20 @@ func rootHandler(proxyPort string) func(http.ResponseWriter, *http.Request) {
     return func(w http.ResponseWriter, r *http.Request) {
         fmt.Println("Request from:", r.RemoteAddr, r.URL.RequestURI())
 
+        q := r.URL.Query()
+        if q.Get("date") != "" {
+            q.Set("year", q.Get("date")[:4])
+            q.Set("month", q.Get("date")[5:7])
+            q.Set("day", q.Get("date")[8:10])
+
+            q.Del("date")
+        }
+
         // Query database
-        resp, err := http.Get("http://127.0.0.1:" + proxyPort +
-            "/covid19-db:1.0/query?" + r.URL.RawQuery)
+        proxyUrlStr := "http://127.0.0.1:" + proxyPort +
+            "/covid19-db:1.0/query?" + q.Encode()
+        fmt.Println("Request to proxy:", proxyUrlStr)
+        resp, err := http.Get(proxyUrlStr)
         if err != nil {
             fmt.Println(err)
             fmt.Fprint(w, err)
@@ -85,6 +89,7 @@ func rootHandler(proxyPort string) func(http.ResponseWriter, *http.Request) {
 }
 
 func faviconHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("Favicon request")
     http.ServeFile(w, r, "favicon.ico")
 }
 
