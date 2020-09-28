871e373c4eb7f24cda1f5b7cb5b6b66f4cedb39b
Ubuntu <29393345+hivanco@users.noreply.github.com>
2020-09-28 02:29:54 -0400

    Sensor service for streaming CPU utilization

--- cpu-usage/cpu_usage.sh
+++ cpu-usage/cpu_usage.sh
@@ -3,6 +3,6 @@
 # Measure CPU utilization every 0.5 seconds
 while true
 do
-    cpu_usage=$(top -bn2 -d 0.5 | fgrep "Cpu(s)" | tail -1 | awk  -F'id,' '{ n=split($1, vals, ","); v=vals[n]; sub("%", "", v); printf "%f", 100 - v }')
+    cpu_usage=$(top -bn2 -d 0.5 | fgrep 'Cpu(s)' | tail -1 | awk  -F'id,' '{ n=split($1, vals, ","); v=vals[n]; sub("%", "", v); printf "%f", 100 - v }')
     echo $cpu_usage
 done
---
+++ cpu-usage/sensor/sensor.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+        fmt.Println("Usage:", os.Args[0], "<proxy-port> <listen-port>")
+        os.Exit(1)
+    }
+    proxyPort := os.Args[1]
+    listenPort := os.Args[2]
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Request from:", r.RemoteAddr)
+		fmt.Fprintf(w, "OK")
+	})
+
+	go func(){
+		log.Fatal(http.ListenAndServe(":" + listenPort, nil))
+	}()
+
+	for {
+		cmd := exec.Command("bash", "-c",
+				`top -bn2 -d 0.5 | fgrep 'Cpu(s)' | tail -1 | awk  -F'id,' '{ n=split($1, vals, ","); v=vals[n]; sub("%", "", v); printf "%f", 100 - v }'`)
+		output, err := cmd.Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		cpuUtilization, err := strconv.ParseFloat(string(output), 32)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("CPU:", cpuUtilization)
+
+		go func() {
+			resp, err := http.Get("http://127.0.0.1:" + proxyPort +
+					"/cpu-usage-aggregator:1.0/" + fmt.Sprintf("%f", cpuUtilization))
+			if err != nil {
+				log.Fatal(err)
+			}
+			resp.Body.Close()
+		}()
+	}
+}
+
+
